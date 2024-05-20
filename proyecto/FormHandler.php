5ce38715da09f5db7a3c467df9b2c426897fb520
Vargas <jeanpaulfuentesvargas@gmail.com>
2024-05-20 10:00:04 -0600

    solicitud y validacion de formulario

--- proyecto/FormHandler.php
+++ proyecto/FormHandler.php
@@ -2,11 +2,43 @@
 
 require_once 'Database.php';
 
-
 class FormHandler {
     private $db; // Instancia de la clase Database
 
-  
+
     public function __construct($db) {
-        $this->db = $db; // Asigna la instancia de Database
+        $this->db = $db; 
     }
+
+
+    public function handleRequest() {
+        // Verifica si la solicitud es de tipo POST
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Obtiene los datos del formulario
+            $nombre_persona = $_POST['nombre_persona'];
+            $menu = $_POST['menu'];
+            $fecha_hora = $_POST['fecha_hora'];
+            $tiempo_comida = $_POST['tiempo_comida'];
+            $nombre_servidor = $_POST['nombre_servidor'];
+
+        
+            if ($this->validate($nombre_persona, $menu, $fecha_hora, $tiempo_comida, $nombre_servidor)) {
+             
+                $this->db->insertData($nombre_persona, $menu, $fecha_hora, $tiempo_comida, $nombre_servidor);
+             
+                echo "<div class='alert alert-success'>Información guardada correctamente.</div>";
+            } else {
+            
+                echo "<div class='alert alert-danger'>Todos los campos son obligatorios.</div>";
+            }
+        }
+    }
+
+ 
+    private function validate($nombre_persona, $menu, $fecha_hora, $tiempo_comida, $nombre_servidor) {
+ 
+        return !empty($nombre_persona) && !empty($menu) && !empty($fecha_hora) && !empty($tiempo_comida) && !empty($nombre_servidor);
+    }
+}
+
+?>
