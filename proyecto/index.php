a8bcb8de0b475520b54230cbf1a90f39dde03808
Vargas <jeanpaulfuentesvargas@gmail.com>
2024-05-20 11:14:54 -0600

    intento de conexion a data studio

--- proyecto/index.php
+++ proyecto/index.php
@@ -3,11 +3,6 @@
 require_once 'Database.php';
 require_once 'FormHandler.php';
 
-
-$db = new Database('curso_honduras', 'hostname', 'service_name', 'username', 'password');
-
-
-$formHandler = new FormHandler($db);
 ?>
 
 <!DOCTYPE html>
